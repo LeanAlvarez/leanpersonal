ab21cb4e5304c03a42cfd67fd09f7ea7c2cc175a
Lean Alvarez <leandro.alvarez8727@gmail.com>
2019-07-04 14:30:33 -0300

    mejorando

--- webpersonal/settings.py
+++ webpersonal/settings.py
@@ -122,7 +122,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
-STATIC_URL = '/static/projects/'
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR,'static')
 
 # STATICFILES_DIRS = (
@@ -133,7 +133,7 @@ STATIC_ROOT = os.path.join(BASE_DIR,'static')
 
 # Media Files
 
-MEDIA_URL = '/media/'
+MEDIA_URL = '/media/projects/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 

e056cbdd5e1e83afc2dc1e8138a4ebd5e8c025df
Lean Alvarez <leandro.alvarez8727@gmail.com>
2019-07-04 14:19:54 -0300

    algunos cambios

--- webpersonal/wsgi.py
+++ webpersonal/wsgi.py
@@ -15,5 +15,4 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "webpersonal.settings")
 
 application = get_wsgi_application()
 
-from whitenoise.django import DjangoWhiteNoise
-application = DjangoWhiteNoise(application)
+
